4935bd2440b177436bc47451a53bb9b2a582070e
Vilislav <vilislavangelov@gmail.com>
2024-10-17 17:50:57 +0300

    Added the basic functionality. The game is playable, but lacks scoring and continuity.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let computerChoice;
 let humanChoice;
+let computerScore = 0;
+let playerScore = 0;
 
 function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
@@ -19,4 +21,58 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function playRound(humanChoice, computerChoice){
+    switch (humanChoice){
+        case "rock":
+            switch (computerChoice){
+                case "Rock":
+                    console.log("Draw!");
+                    break;
+                case "Paper":
+                    console.log("You Lose!");
+                    computerScore += 1;
+                    break;
+                case "Scissors":
+                    console.log("You Win!");
+                    playerScore += 1;
+                    break;
+            }
+            break;
+        case "paper":
+            switch (computerChoice){
+                case "Rock":
+                    console.log("You Win!");
+                    playerScore += 1;
+                    break;
+                case "Paper":
+                    console.log("Draw!");
+                    break;
+                case "Scissors":
+                    console.log("You Lose!");
+                    computerScore += 1;
+                    break;
+            }
+            break;
+        case "scissors":
+            switch (computerChoice){
+                case "Rock":
+                    console.log("You Lose!");
+                    computerScore += 1;
+                    break;
+                case "Paper":
+                    console.log("You Win!");
+                    playerScore += 1;
+                    break;
+                case "Scissors":
+                    console.log("Draw!");
+                    break;
+            }
+            break;
+
+    }
+
+}
+
+humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+getComputerChoice();
+playRound(humanChoice, computerChoice);

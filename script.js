dbf71f5b4480f56052e1726ae3e32c0c614056b2
Vilislav <vilislavangelov@gmail.com>
2024-10-22 16:55:39 +0300

    Simplified game logic and cleaned up the code.

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ let humanChoice;
 let computerScore = 0;
 let playerScore = 0;
 
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
+
 while (computerScore != 5 && playerScore != 5) {
     humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
-    getComputerChoice();
+    computerChoice = getComputerChoice();
     playRound(humanChoice, computerChoice);
     console.log(`Computer chose: ${computerChoice}.`);
     console.log(`SCORE: Player ${playerScore} | Computer ${computerScore}`);
@@ -19,69 +24,23 @@ if (computerScore == 5) {
 }
 
 function getComputerChoice() {
-    computerChoice = Math.floor(Math.random() * (3 - 1 + 1) + 1);
-    switch (computerChoice) {
-        case 1:
-            computerChoice = 'Rock';
-            return computerChoice;
-            break;
-        case 2:
-            computerChoice = 'Paper';
-            return computerChoice;
-            break;
-        case 3:
-            computerChoice = 'Scissors';
-            return computerChoice;
-            break;
-    }
+    const choices = [ROCK, PAPER, SCISSORS];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
 }
 
 function playRound(humanChoice, computerChoice){
-    switch (humanChoice){
-        case "rock":
-            switch (computerChoice){
-                case "Rock":
-                    console.log("Draw!");
-                    break;
-                case "Paper":
-                    console.log("You Lose!");
-                    computerScore += 1;
-                    break;
-                case "Scissors":
-                    console.log("You Win!");
-                    playerScore += 1;
-                    break;
-            }
-            break;
-        case "paper":
-            switch (computerChoice){
-                case "Rock":
-                    console.log("You Win!");
-                    playerScore += 1;
-                    break;
-                case "Paper":
-                    console.log("Draw!");
-                    break;
-                case "Scissors":
-                    console.log("You Lose!");
-                    computerScore += 1;
-                    break;
-            }
-            break;
-        case "scissors":
-            switch (computerChoice){
-                case "Rock":
-                    console.log("You Lose!");
-                    computerScore += 1;
-                    break;
-                case "Paper":
-                    console.log("You Win!");
-                    playerScore += 1;
-                    break;
-                case "Scissors":
-                    console.log("Draw!");
-                    break;
-            }
-            break;
+    if (humanChoice === computerChoice.toLowerCase()) {
+        console.log("Draw!");
+    } else if (
+        (humanChoice === ROCK && computerChoice === SCISSORS) ||
+        (humanChoice === PAPER && computerChoice === ROCK) ||
+        (humanChoice === SCISSORS && computerChoice === PAPER)
+    ) {
+        console.log("You Win!");
+        playerScore++;
+    } else {
+        console.log("You Lose!");
+        computerScore++;
     }
 }

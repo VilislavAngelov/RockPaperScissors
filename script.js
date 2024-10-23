7c64de50ef3b2f46f53dd4519bfae984ffd8a5d8
Vilislav <vilislavangelov@gmail.com>
2024-10-23 07:19:18 +0300

    Making good progress, successfully made rock,
    paper and scissors signs and now I'm working on
    the game logic.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//TODO
+//make a function to make scissors sign
+//make the shake function
+//attach computerChoice to the functions to show choice
+//clone the hand to use it as playerHand
+//be able to enter input myself
+//make the buttons to choose R P or S
+//be able to play a round
+//Add text about whos winning
+//Track the score
+
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
@@ -17,6 +28,7 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 
 const loader = new GLTFLoader();
 let bones = [];
+const originalRotations = [];
 
 loader.load('RiggedArms.glb', function (gltf) {
     computerHand = gltf.scene;
@@ -24,42 +36,154 @@ loader.load('RiggedArms.glb', function (gltf) {
 
     computerHand.traverse(function (object) {
         if (object.isBone) {
-
           bones.push(object)
         }
     });
 
-    bones[12].rotation.x += 0.5;
-    computerHand.position.set(0, -2, 2);
-    computerHand.rotateY(Math.PI / 1);
-    computerHand.rotateX(Math.PI / 1);
-    computerHand.rotateZ(Math.PI / 1);
+
+    computerHand.position.set(5 , 0, 2);
+    computerHand.rotation.set(0, 0, 0);
+    computerHand.rotation.order = 'YZX';
+    computerHand.rotation.y = Math.PI;
+    computerHand.rotation.z = Math.PI / 2;
+    computerHand.rotation.x = Math.PI;
+    
+    makeFist();
+
+    gsap.delayedCall(1, makePaper);
+
+    gsap.delayedCall(2, resetToFist);
+
+    gsap.delayedCall(3, makeScissors);
+
+    gsap.delayedCall(4, resetToFist);
+
+    
 }, undefined, function (error) {
     console.error( error);
 }) 
 
 const renderer = new THREE.WebGLRenderer;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild(renderer.domElement);
 
 const geometryWall = new THREE.PlaneGeometry( 20, 20 );
-const materialWall = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+const materialWall = new THREE.MeshBasicMaterial( {color: 0x9fc6fc, side: THREE.DoubleSide} );
 const wall = new THREE.Mesh( geometryWall, materialWall );
 scene.add( wall );
 
 const geometryFloor = new THREE.PlaneGeometry( 20, 20 );
-const materialFloor = new THREE.MeshBasicMaterial( {color: 0x00ff00, side: THREE.DoubleSide} );
+const materialFloor = new THREE.MeshBasicMaterial( {color: 0x89aad9, side: THREE.DoubleSide} );
 const floor = new THREE.Mesh( geometryFloor, materialFloor );
 scene.add( floor );
 floor.rotation.x = -0.5 * Math.PI;
 floor.position.y = -1.5;
 
-const light = new THREE.AmbientLight( 0xffffff ); // soft white light
-scene.add( light );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 4 );
+directionalLight.position.set( 0, 15, 5 ); //default; light shining from top
+directionalLight.castShadow = true; // default false
+scene.add( directionalLight );
 
-camera.position.z = 20;
+const helper = new THREE.DirectionalLightHelper( directionalLight, 5 );
+scene.add( helper );
+
+
+camera.position.z = 10;
 
 const controls = new OrbitControls( camera, renderer.domElement );
+
+function makeFist() {
+    // Animate thumb bones
+    gsap.to(bones[3].rotation, { z: bones[3].rotation.z - 0.5, duration: 0.5 });
+    gsap.to(bones[4].rotation, { y: -1, duration: 0.5 });
+    gsap.to(bones[5].rotation, { z: bones[5].rotation.z - 1.3, duration: 0.5 });
+
+// Animate index finger bones
+    gsap.to(bones[7].rotation, { z: bones[7].rotation.z - 1.3, duration: 0.5 });
+    gsap.to(bones[8].rotation, { z: bones[8].rotation.z - 1, duration: 0.5 });
+    gsap.to(bones[9].rotation, { z: bones[9].rotation.z - 1, duration: 0.5 });
+
+// Animate middle finger bones
+    gsap.to(bones[11].rotation, { x: bones[11].rotation.x + 1.3, duration: 0.5 });
+    gsap.to(bones[12].rotation, { x: bones[12].rotation.x + 1, duration: 0.5 });
+    gsap.to(bones[13].rotation, { x: bones[13].rotation.x + 1, duration: 0.5 });
+
+// Animate ring finger bones
+    gsap.to(bones[15].rotation, { z: bones[15].rotation.z + 1.3, duration: 0.5 });
+    gsap.to(bones[16].rotation, { z: bones[16].rotation.z + 1, duration: 0.5 });
+    gsap.to(bones[17].rotation, { z: bones[17].rotation.z + 1, duration: 0.5 });
+
+// Animate pinky finger bones
+    gsap.to(bones[19].rotation, { z: bones[19].rotation.z + 1.3, duration: 0.5 });
+    gsap.to(bones[20].rotation, { z: bones[20].rotation.z + 1, duration: 0.5 });
+    gsap.to(bones[21].rotation, { z: bones[21].rotation.z + 1, duration: 0.5 });
+
+    bones.forEach((bone, index) => {
+        originalRotations[index] = {
+            x: bone.rotation.x,
+            y: bone.rotation.y,
+            z: bone.rotation.z,
+        }
+    })
+
+}
+
+function resetToFist() {
+    const duration = 0.5; // Animation duration in seconds
+  
+    bones.forEach((bone, index) => {
+      gsap.to(bone.rotation, {
+        x: originalRotations[index].x,
+        y: originalRotations[index].y,
+        z: originalRotations[index].z,
+        duration,
+      });
+    });
+}
+  
+
+function makePaper() {
+    gsap.to(bones[3].rotation, { z: bones[3].rotation.z + 0.5, duration: 0.5 });
+    gsap.to(bones[4].rotation, { y: bones[4].rotation.y + 1, duration: 0.5 });
+    gsap.to(bones[5].rotation, { z: bones[5].rotation.z + 1.3, duration: 0.5 });
+
+    // Animate index finger bones
+    gsap.to(bones[7].rotation, { z: bones[7].rotation.z + 1.3, duration: 0.5 });
+    gsap.to(bones[8].rotation, { z: bones[8].rotation.z + 1, duration: 0.5 });
+    gsap.to(bones[9].rotation, { z: bones[9].rotation.z + 1, duration: 0.5 });
+
+    // Animate middle finger bones
+    gsap.to(bones[11].rotation, { x: bones[11].rotation.x - 1.3, duration: 0.5 });
+    gsap.to(bones[12].rotation, { x: bones[12].rotation.x - 1, duration: 0.5 });
+    gsap.to(bones[13].rotation, { x: bones[13].rotation.x - 1, duration: 0.5 });
+
+    // Animate ring finger bones
+    gsap.to(bones[15].rotation, { z: bones[15].rotation.z - 1.3, duration: 0.5 });
+    gsap.to(bones[16].rotation, { z: bones[16].rotation.z - 1, duration: 0.5 });
+    gsap.to(bones[17].rotation, { z: bones[17].rotation.z - 1, duration: 0.5 });
+
+    // Animate pinky finger bones
+    gsap.to(bones[19].rotation, { z: bones[19].rotation.z - 1.3, duration: 0.5 });
+    gsap.to(bones[20].rotation, { z: bones[20].rotation.z - 1, duration: 0.5 });
+    gsap.to(bones[21].rotation, { z: bones[21].rotation.z - 1, duration: 0.5 });
+
+}
+
+function makeScissors() {
+
+    // Animate index finger bones
+    gsap.to(bones[7].rotation, { z: bones[7].rotation.z + 1.3, duration: 0.5 });
+    gsap.to(bones[8].rotation, { z: bones[8].rotation.z + 1, duration: 0.5 });
+    gsap.to(bones[9].rotation, { z: bones[9].rotation.z + 1, duration: 0.5 });
+
+    // Animate middle finger bones
+    gsap.to(bones[11].rotation, { x: bones[11].rotation.x - 1.3, duration: 0.5 });
+    gsap.to(bones[12].rotation, { x: bones[12].rotation.x - 1, duration: 0.5 });
+    gsap.to(bones[13].rotation, { x: bones[13].rotation.x - 1, duration: 0.5 });
+
+}
 
 function animate() {
 	renderer.render( scene, camera );

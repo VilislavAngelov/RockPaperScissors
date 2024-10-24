1a12c3c7ef7c6a2f4d42fa53049c51c7678397bd
Vilislav <vilislavangelov@gmail.com>
2024-10-24 21:43:38 +0300

    bugfixing deploy v2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/RockPaperScissors/", // Set the base URL
+  base: "/RockPaperScissors/", // Set the base URL for GitHub Pages deployment
   resolve: {
     alias: {
-      'three': 'three/build/three.module.js',
+      'three': 'three', // Vite will resolve this automatically from node_modules
       'three/examples': 'three/examples/jsm', // Alias for examples
     }
   }
 });
+
